b406b852b6384ccb8d82bae332093573d0c0cdcb
Vr6R32 <121500661+Vr6R32@users.noreply.github.com>
2023-09-10 17:36:59 +0200

    small cleaning

--- module-main/mail-module/src/main/java/pl/motobudzet/mailingmodule/MailingModuleApplication.java
+++ module-main/mail-module/src/main/java/pl/motobudzet/mailingmodule/MailingModuleApplication.java
@@ -1,13 +1,8 @@
 package pl.motobudzet.mailingmodule;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 @EnableKafka
