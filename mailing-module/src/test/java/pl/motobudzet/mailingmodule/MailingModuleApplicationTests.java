9f7c27a2d621d447859e03dc43a6c996f363976b
Vr6R32 <121500661+Vr6R32@users.noreply.github.com>
2023-09-10 09:27:10 +0200

    mail module implemented

---
+++ mailing-module/src/test/java/pl/motobudzet/mailingmodule/MailingModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.motobudzet.mailingmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailingModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
